b762b287b25b7f14e4d6c6425ed4de7abfd6f161
ManuelGali <131902347+ManuelGali@users.noreply.github.com>
2023-04-26 13:14:16 -0600

    Update IndexController.java

--- src/main/java/com/github/hackathon/advancedsecurityjava/Controllers/IndexController.java
+++ src/main/java/com/github/hackathon/advancedsecurityjava/Controllers/IndexController.java
@@ -2,9 +2,9 @@ package com.github.hackathon.advancedsecurityjava.Controllers;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
